fb854060b1a710781b1b58644c2267b7588b4ec1
Soham Tilekar <sohamtilekar233@gmail.com>
2024-04-18 14:20:38 +0530

    Refactor main.cpp to read file content from command-line argument

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
-#include <iostream>
-#include "lexer.hpp"
+#include "include/lexer.hpp"
 
-int main()
+int main(int argc, char *argv[])
 {
+    if (argc < 2)
+    {
+        std::cout << "Please provide a file path as a command-line parameter." << std::endl;
+        return 1;
+    }
+
+    std::string filePath = argv[1];
+    std::ifstream file(filePath);
+
+    if (!file)
+    {
+        std::cout << "Failed to open the file." << std::endl;
+        return 1;
+    }
+
+    std::string fileContent((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+
+    std::cout << "File content: \"" << fileContent << "\"\n";
 
     return 0;
 }
