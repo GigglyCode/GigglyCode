9eed476c0e2457c75e08454afd723fa52a20ab30
Soham Tilekar <sohamtilekar233@gmail.com>
2024-05-01 16:13:43 +0530

    Add main.cpp file with lexer debug code

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "include/lexer.hpp"
+
+bool DEBUG_LEXER = true;
+
+int main(int argc, char *argv[])
+{
+    // Reading code
+    if (argc != 2)
+        return 1;
+
+    std::ifstream file(argv[1]);
+    if (!file)
+        return 1;
+
+    std::string fileContent((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    // Lexer
+    if (DEBUG_LEXER)
+    {
+        std::cout << "=========== Lexer Debug ===========" << std::endl;
+        Lexer debugLexer(fileContent);
+        while (debugLexer.currentChar != "\0")
+        {
+            std::shared_ptr<token::Token> token = debugLexer.nextToken();
+            token->print();
+        }
+        return 0;
+    }
+}
