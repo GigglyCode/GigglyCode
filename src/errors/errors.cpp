1023619164e46ec660d00df6c93c521dcec488e4
Soham Tilekar <sohamtilekar233@gmail.com>
2024-05-08 22:18:06 +0530

    Refactor errors module, add SyntaxError and NoPrefixParseFnError classes

--- src/errors/errors.cpp
+++ src/errors/errors.cpp
@@ -2,46 +2,128 @@
 #include <vector>
 #include "errors.hpp"
 
-void raiseSyntaxError(std::string source, std::string message, int line_no, int col_no, std::string literal, std::string errorType, std::string suggestedFix)
+void errors::Error::raise(bool terminate)
 {
     std::cerr << "\n\n\033[1;35m" << std::string(60, '=') << "\033[0m\n\n";
 
     // Print error message in red color
     std::cerr << "\033[1;31m"
-              << "SyntaxError." << errorType << ": \033[0m"
+              << "Error: \033[0m"
               << "\033[1;97m" << message << "\033[0m\n";
 
-    if (line_no != -1)
+    if (st_line != -1 && end_line != -1)
     {
         std::cerr << "\033[1;36mSource Context:\033[0m\n";
 
-        if (line_no - 1 > 0)
+        if (st_line - 1 > 0)
         {
-            std::cerr << "\033[0;32m" << line_no - 1 << " | \033[0m" << getStringOnLineNumber(source, line_no - 1) << "\n";
+            std::cerr << "\033[0;32m" << st_line - 1 << " | \033[0m" << getStringOnLineNumber(source, st_line - 1) << "\n";
         }
-        std::istringstream iss(literal);
-        std::vector<std::string> lines;
-        int lit_line_no = line_no;
-        std::stringstream concatenated_lines;
-        std::string line;
-        while (std::getline(iss, line))
+        for (int i = st_line; i <= end_line; i++)
         {
-            concatenated_lines << (lit_line_no == line_no ? "\033[1;31m\033[4m" : "\033[0;32m" + std::to_string(lit_line_no) + " | \033[1;31m\033[4m") << line << "\033[0m\n";
-            lit_line_no++;
+            std::cerr << "\033[0;32m" << i << " | \033[0m" << getStringOnLineNumber(source, i) << "\n";
         }
-        literal = concatenated_lines.str();
-        line = (getStringOnLineNumber(source, line_no).substr(0, col_no - 1) + literal + "\033[0m");
-        std::cerr << "\033[0;32m" << line_no << " | \033[0m" << line << "\n";
-        if (line_no + 1 <= getNumberOfLines(source) && lit_line_no == line_no)
+        if (end_line + 1 <= getNumberOfLines(source))
         {
-            std::cerr << "\033[0;32m" << line_no + 1 << " | \033[0m: " << getStringOnLineNumber(source, line_no + 1) << "\n";
+            std::cerr << "\033[0;32m" << end_line + 1 << " | \033[0m" << getStringOnLineNumber(source, end_line + 1) << "\n";
         }
     }
+
     //  Print suggested fix if it's not empty
     if (!suggestedFix.empty())
     {
         std::cerr << "\033[1;33m"
                   << "Suggested fix: " << suggestedFix << "\033[0m\n\n";
     }
-    exit(EXIT_FAILURE);
+    if (terminate)
+        exit(EXIT_FAILURE);
+}
+void errors::NoPrefixParseFnError::raise(bool terminate)
+{
+    std::cerr << "\n\n\033[1;35m" << std::string(60, '=') << "\033[0m\n\n";
+
+    // Print error message in red color
+    std::cerr << "\033[1;31m"
+              << "NoPrefixParseFnError: \033[0m"
+              << "\033[1;97m" << message << "\033[0m\n";
+
+    std::cerr << "\033[1;36mSource Context:\033[0m\n";
+
+    if (token.line_no - 1 >= 0)
+    {
+        std::cerr << "\033[0;32m" << token.line_no - 1 << " | \033[0m" << getStringOnLineNumber(source, token.line_no - 1) << "\n";
+    }
+    int litLineNo = token.line_no;
+    std::stringstream concatenatedLines;
+    std::string line;
+    while (std::getline(std::istringstream(token.literal), line))
+    {
+        concatenatedLines << (litLineNo == token.line_no ? "\033[1;31m\033[4m" : "\033[0;32m" + std::to_string(litLineNo) + " | \033[1;31m\033[4m") << line << "\033[0m\n";
+        litLineNo++;
+    }
+    std::string literal = concatenatedLines.str();
+    line = (getStringOnLineNumber(source, token.line_no).substr(0, token.col_no - 1) + literal + "\033[0m");
+    std::cerr << "\033[0;32m" << token.line_no << " | \033[0m" << line << "\n";
+
+    if (token.line_no + 1 <= getNumberOfLines(source) && litLineNo == token.line_no)
+    {
+        std::cerr << "\033[0;32m" << token.line_no + 1 << " | \033[0m" << getStringOnLineNumber(source, token.line_no + 1) << "\n";
+    }
+
+    //  Print suggested fix if it's not empty
+    if (!suggestedFix.empty())
+    {
+        std::cerr << "\033[1;33m"
+                  << "Suggested fix: " << suggestedFix << "\033[0m\n\n";
+    }
+    if (terminate)
+        exit(EXIT_FAILURE);
+}
+
+void errors::SyntaxError::raise(bool terminate)
+{
+    std::cerr << "\n\n\033[1;35m" << std::string(60, '=') << "\033[0m\n\n";
+
+    // Print error message in red color
+    std::cerr << "\033[1;31m"
+              << "SyntaxError: \033[0m"
+              << "\033[1;97m" << message << "\033[0m\n";
+
+    std::cerr << "\033[1;36mSource Context:\033[0m\n";
+
+    if (token.line_no - 1 >= 0)
+    {
+        std::cerr << "\033[0;32m" << token.line_no - 1 << " | \033[0m" << getStringOnLineNumber(source, token.line_no - 1) << "\n";
+    }
+    int litLineNo = token.line_no;
+    std::stringstream concatenatedLines;
+    std::string line;
+    while (std::getline(std::istringstream(token.literal), line))
+    {
+        concatenatedLines << (litLineNo == token.line_no ? "\033[1;31m\033[4m" : "\033[0;32m" + std::to_string(litLineNo) + " | \033[1;31m\033[4m") << line << "\033[0m\n";
+        litLineNo++;
+    }
+    std::string literal = concatenatedLines.str();
+    line = (getStringOnLineNumber(source, token.line_no).substr(0, token.col_no - 1) + literal + "\033[0m");
+    std::cerr << "\033[0;32m" << token.line_no << " | \033[0m" << line << "\n";
+
+    if (token.line_no + 1 <= getNumberOfLines(source) && litLineNo == token.line_no)
+    {
+        std::cerr << "\033[0;32m" << token.line_no + 1 << " | \033[0m" << getStringOnLineNumber(source, token.line_no + 1) << "\n";
+    }
+
+    //  Print suggested fix if it's not empty
+    if (!suggestedFix.empty())
+    {
+        std::cerr << "\033[1;33m"
+                  << "Suggested fix: " << suggestedFix << "\033[0m\n\n";
+    }
+    if (terminate)
+        exit(EXIT_FAILURE);
+}
+
+void errors::raiseSyntaxError(std::string source, token::Token token, std::string message, std::string suggestedFix)
+{
+    SyntaxError error(source, token, message, suggestedFix);
+    error.raise();
 }
--- src/errors/errors.hpp
+++ src/errors/errors.hpp
@@ -2,5 +2,36 @@
 #include "../lexer/tokens.hpp"
 #include "../lexer/lexer.hpp"
 
-// void raiseLexerError(std::string message, std::string source, int line_no, int col_no, std::string literal);
-void raiseSyntaxError(std::string source, std::string message = "", int line_no = -1, int col_no = -1, std::string literal = "", std::string errorType = "", std::string suggestedFix = "");
+namespace errors
+{
+    class Error
+    {
+    public:
+        std::string source;
+        int st_line;
+        int end_line;
+        std::string message;
+        std::string suggestedFix;
+        Error(std::string source, int st_line, int end_line, std::string message, std::string suggestedFix = "") : source(source), st_line(st_line), end_line(end_line), message(message), suggestedFix(suggestedFix) {}
+        Error(){};
+        virtual void raise(bool terminate = true);
+    };
+
+    class SyntaxError : public Error
+    {
+    public:
+        token::Token token;
+        SyntaxError(std::string source, token::Token token, std::string message = "", std::string suggestedFix = "") : Error(source, -1, -1, message, suggestedFix), token(token) {}
+        void raise(bool terminate = true);
+    };
+
+    void raiseSyntaxError(std::string source, token::Token token, std::string message = "", std::string suggestedFix = "");
+
+    class NoPrefixParseFnError : public Error
+    {
+    public:
+        token::Token token;
+        NoPrefixParseFnError(std::string source, token::Token token, std::string message = "", std::string suggestedFix = "") : Error(source, -1, -1, message, suggestedFix), token(token) {}
+        void raise(bool terminate = true);
+    };
+}
